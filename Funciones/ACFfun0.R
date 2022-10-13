49474d57ff75e46f190b091e68227539ee7c57f7
Manuel Corredor <macamilo2893@gmail.com>
2022-10-13 02:16:18 -0300

    Uptade

--- Funciones/ACFfun0.R
+++ Funciones/ACFfun0.R
@@ -1,6 +1,6 @@
 
 
-
+#tiene en cuenta la posicion 0
 ACFfun0 <- function(Z,K){
   media <- mean(Z)
   N <- length(Z)
@@ -31,5 +31,21 @@ ACFfun00 <- function(Z,K){
     ACF[k+1] <- numerador/denominador
   }
   plot(ACF,type="h")
+  return(ACF)
+}
+
+# omite el valor en la posicion 0
+ACFfun <- function(Z,K){
+  media <- mean(Z)
+  N <- length(Z)
+  ACF <- matrix(data = NA,nrow = (K),ncol = 1)#creación vector vacio
+  for(k in 1:K){
+    Zk <- Z[(k+1):N] # valor eliminados al inicio
+    Zklag <- Z[1:(N-k)] #valores eliminados al final
+    numerador <- sum((Zk-media)*(Zklag-media))
+    denominador <- sum((Z-media)^2)
+    ACF[k] <- numerador/denominador
+  }
+  #plot(ACF,type="h")
   return(ACF)
 }
